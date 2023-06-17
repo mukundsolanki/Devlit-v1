a600b20d4f087f168dd3a27cb984ca2028918e30
Mukund Solanki <mukundsolanki665@gmail.com>
2023-06-17 23:57:31 +0530

    Created new components

---
+++ src/components/Build.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header';
+
+export default function Build() {
+  return (
+    <div>
+      <Header/>
+      
+    </div>
+  )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import Cursor from './Cursor.jsx';
+import Header from './Header.jsx';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className='home-links'>
-      <ul>
-        <li>
-          <a>Start Building</a>
-        </li>
-        <li>
-          <a>Try our online compiler</a>
-        </li>
-        <li>
-          <a>Your Profile</a>
-        </li>
-        <li>
-          <a>About Devlit</a>
-        </li>
-        <li>
-          <a>Contact us</a>
-        </li>
-      </ul>
-      <Cursor />
-    </div>
+    <>
+      <Header />
+      <div className='home-links'>
+        <ul>
+          <li>
+            <Link to="/build">Start Building</Link>
+          </li>
+          <li>
+            <Link to="/compiler">Try our online compiler</Link>
+          </li>
+          <li>
+            <Link to="/profile">Your Profile</Link>
+          </li>
+          <li>
+            <Link to="/about">About Devlit</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+        </ul>
+        <Cursor />
+      </div>
+    </>
   );
 }
